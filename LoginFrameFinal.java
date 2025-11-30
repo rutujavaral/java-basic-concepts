39b99403cb86f243af44aea574640cd9c55e4f28
Rutuja Varal <rutujavaral660@gmail.com>
2025-11-30 15:36:27 +0530

    Add files via upload

---
+++ LoginFrameFinal.java
@@ -0,0 +1,87 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+class MarvellousLogin implements ActionListener
+{
+    //characteristics
+     JFrame fobj;
+     JButton bobj;
+     JTextField tobj;
+     JPasswordField pobj;
+     JLabel UserLabel,Passlable,Resultlabel;
+
+
+   public MarvellousLogin( String title,int width,int height)
+   {
+    fobj = new JFrame("title");
+    
+
+    UserLabel=new JLabel("user name");  
+    UserLabel.setBounds(50,50,100,30);
+
+
+    tobj=new JTextField();
+    tobj.setBounds(150,50,150,30);
+
+    Passlable=new JLabel("Password");
+    Passlable.setBounds(50,100,100,30);
+
+    pobj = new JPasswordField();                    
+    pobj.setBounds(150,100,150,30);
+
+    bobj= new JButton("SUBMIT");
+    bobj.setBounds(150,150,100,30);
+
+
+
+    Resultlabel=new JLabel("");
+    Resultlabel.setBounds(130,200,250,30);
+
+    
+    fobj.add(bobj);
+    fobj.add(tobj);
+    fobj.add(pobj);
+    fobj.add(UserLabel);
+    fobj.add(Passlable);
+    fobj.add(Resultlabel);
+
+    
+    
+    bobj.addActionListener(this);
+    
+
+    fobj.setLayout(null);
+    fobj.setSize(width,height);
+
+    fobj.setVisible(true);
+    fobj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+   }
+
+   //Username:marvellous
+   //Password:Marvellous@123
+
+   public void actionPerformed(ActionEvent aobj)
+   {
+       String uname = tobj.getText();
+       String pass =  pobj.getText();
+
+       if((uname.equals("Marvellous")) && (pass.equals("Marvellous@123")))
+       {
+           Resultlabel.setText("Login Sucessfull....");
+       }
+       else
+       {
+           Resultlabel.setText("Login failed.....");
+         
+       }
+   }
+}
+class LoginFrameFinal
+{
+    public static void main(String A[])
+    {
+
+           MarvellousLogin mobj = new MarvellousLogin("Login",400,300);
+
+    }
+}
